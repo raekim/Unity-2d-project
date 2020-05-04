1b19b97f8a316576f65fa9029782c815bb85ce29
RaeKim <raekim95@gmail.com>
2020-05-04 16:38:57 +0900

    Initial commit

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : PhysicsObject
+{
+    public Animator animator;
+    bool playerNear;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetVelocity = Vector2.right;
+
+        animator.SetBool("Walking", true);
+    }
+    
+}
